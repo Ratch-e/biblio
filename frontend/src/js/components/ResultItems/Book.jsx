cb2ef1db7875218e560f09379debd29494d3aa6f
Viktor Gunin <guninvk@gmail.com>
2018-11-04 00:38:32 +0400

    refactored book model

--- frontend/src/js/components/ResultItems/Book.jsx
+++ frontend/src/js/components/ResultItems/Book.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
-const Book = ({ authors, title, city, publisher, year, pages }) => (
-  <span className="list__text">
-    {authors.map(
-      (author, key) =>
-        authors.length - 1 === key
-          ? `${author.lastname} ${author.name[0]}.${author.middlename[0]}`
-          : `${author.lastname} ${author.name[0]}.${author.middlename[0]}, `,
-    )}
-    . {title}. &mdash; {city === 'Москва' ? 'M.' : city === 'Cанкт-Петербург' ? 'СПб.' : city}:{' '}
-    {publisher}, {year}. &mdash; {pages}
-    c.
-  </span>
-);
+const Book = ({ authors, title, city, publisher, year, pages }) => {
+  const makeCity = (city) => {
+    switch(city) {
+      case 'Москва':
+        return 'M.'
+
+      case 'Санкт-Петербург':
+        return 'СПб.'
+
+      default:
+        return city;
+    }
+  }
+  
+  return (<div className="list__text">
+    {authors.map((author, key) =>
+      authors.length - 1 === key ? 
+      `${author.lastname} ${author.name[0]}.${author.middlename[0]}` : 
+      `${author.lastname} ${author.name[0]}.${author.middlename[0]}, `)}. {title}. &mdash; {makeCity(city)}: {publisher}, {year}. &mdash; {pages}c.
+  </div>);
+};
 
 export default Book;
